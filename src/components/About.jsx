a6e27e2154f08b5aa01730698a1d452aeecf14dd
Bryan Trujillo <trujillobryan99@yahoo.com>
2023-03-09 14:21:35 -0500

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import Tilt from 'react-tilt'
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+import { SectionWrapper } from '../hoc';
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div 
+        variants={fadeIn("right", "spring", 0.5 * 
+        index, 0.75)}
+        className='w-full green-pink-gradient p-
+        [1px] rounded-[20px] shadow-card'
+      >
+      <div
+      options={{
+        max: 45,
+        scale: 1,
+        speed: 450
+      }}
+      className='bg-tertiary rounded-[20px] 
+      py-5 px-12 min-h-[280px] flex
+      justify-evenly items-center flex-col'
+      >
+        <img src={icon} alt={title} 
+        className='w-16 h-16 object-contain' />
+        <h3 className='text-white text-[20px] 
+        font-bold text-center'>{title}</h3>
+      </div>
+      </motion.div>
+    </Tilt>
+  )
+}
+
+const About = () => {
+  return (
+    <>
+      <motion.div>
+        <p className={styles.sectionSubText}
+        >Introduction</p>
+        <h2 className={styles.sectionHeadText}
+        >Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] 
+        max-w-3xl leading-[30px]'
+        >
+        I began my journey into programming while pursuing my degree. 
+        Through self-study and a web development bootcamp course, I grew a passion for creating 
+        applications in languages such as TypeScript/JavaScript and 
+        frameworks like React & Node.js. I recently started contributing to 
+        open source on tampadevs github which is my local meetup group of developers. 
+        I have worked on a variety of projects using the latest technologies. 
+        I am a quick learner and have a strong work ethic. I am always looking to 
+        improve my skills and learn new technologies. Fluent in Spanish as well. 
+        </motion.p>
+       
+       <div className='mt-20 flex flex-wrap gap-10'>
+          {services.map((service, index) => (
+            <ServiceCard key={service.title} index={index} {...service} />
+          ))}
+       </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, 'about')
---
+++ src/constants/index.js
@@ -0,0 +1,246 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+    
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "Backend Developer",
+      icon: backend,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+ 
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    {
+      name: "docker",
+      icon: docker,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "Broward College",
+      company_name: "Associate Degree",
+      icon: starbucks,
+      iconBg: "#383E56",
+      date: "Graduated - June 2013",
+      points: [
+        " Completed courses: Introduction to Business, Accounting Principles, Marketing Fundamentals, and several other math classes (algebra, statistics and calculus).",
+        " Computer courses that taught me Office 365 (Word, Excel, PowerPoint, and Microsoft Teams."
+      ],
+    },
+    {
+      title: "Florida Atlantic University",
+      company_name: "Bachelor Degree in Management Information Systems",
+      icon: tesla,
+      iconBg: "#E6DEDD",
+      date: "Graduated - Aug 2017",
+      points: [
+        "12 credits of Management Information System courses, including Intro to Computer System & Software Development, Database Management Systems, Introduction to Business Intelligence, and Computer Forensics.",
+        "51 credits of core business courses, including Principles of Accounting, Business Finance, Business Information Tools, and Business Management.",
+        "Completed Coding projects in Java and SQL in order to graduate."
+        
+      ],
+    },
+    {
+      title: "Technical Support Specialist",
+      company_name: "Digital South",
+      icon: shopify,
+      iconBg: "#383E56",
+      date: "Oct 2016 - Dec 2017",
+      points: [
+        "Worked in a team environment.",
+        "Organized assigned ticket queues by prioritizing critical issues and effectively managing time to address tasks in a time-sensitive manner.",
+        "Provided technical support to internal/external customers for VoIP-related incidents, requests, and inquiries relating to various business products and services.",
+        "Programmed VOIP phones and handle installation process at clients location.",
+      ],
+    },
+    {
+      title: "(Pre-Training Program) Software Developer",
+      company_name: "Revature",
+      icon: meta,
+      iconBg: "#E6DEDD",
+      date: "Jan 2023 - Present",
+      points: [
+        "Virtual learning program with trainer and primary mentor to guide me through the process.",
+        "Using GitPod to complete coding assignments, coding challenges, and projects to complete Revature Curriculum.",
+        "Learning fundamentals of Java, SQL, GIT, Linux commands, and interview preparation.",
+        "Program designed to guide me qualify for a career Software Engineer."
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "Great implementation by @btruj. Thanks Bryan! - (regarding Issue #50 open source contribution I completed on tampadevs github.)" ,
+      name: "Charlton Trezevant",
+      designation: "Vice President",
+      company: "TampaDevs",
+      image: creator,
+    },
+    {
+      testimonial:
+        "I want you to know I thought you did a great job of helping me with the things I asked of you.",
+      name: "Dave White",
+      designation: "President",
+      company: "Digital South",
+      image: shopify,
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "My Daily Journal",
+      description:
+        "Saves journal entries in MongoDB database for users.",
+      tags: [
+        {
+          name: "EJS",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "JavaScript",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: carrent,
+      source_code_link: "my-daily-journal.onrender.com/",
+    },
+    {
+      name: "Newsletter Sign-Up",
+      description:
+        "MailChimp API is used for this project and user gets saved to Mailchimp database.",
+      tags: [
+        {
+          name: "HTML",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "restapi",
+          color: "green-text-gradient",
+        },
+        {
+          name: "CSS",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jobit,
+      source_code_link: "https://newsletter-nji3.onrender.com/",
+    },
+    {
+      name: "Todo-List",
+      description:
+        "Saves specific list in MongoDB database.",
+      tags: [
+        {
+          name: "JavaScript",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "EJS",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: tripguide,
+      source_code_link: "https://todo-list-3fhr.onrender.com/",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
+  
